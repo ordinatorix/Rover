5d163358b672fbca368b53cc4e463b8570b109e7
brown <yellow@browns-MacBook-Pro.local>
2020-01-25 16:34:29 -0500

    controller_setup file added

--- Adeept_RaspTank/controller_setup.py
+++ Adeept_RaspTank/controller_setup.py
@@ -54,13 +54,13 @@ for x in range(1,4):
 	if os.system("sudo pip3 install -U pip") == 0:
 		break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install numpy") == 0:
-		break
+# for x in range(1,4):
+# 	if os.system("sudo pip3 install numpy") == 0:
+# 		break
 
-for x in range(1,4):
-	if os.system("sudo apt-get install -y libopencv-dev python3-opencv") == 0:
-		break
+# for x in range(1,4):
+# 	if os.system("sudo apt-get install -y libopencv-dev python3-opencv") == 0:
+# 		break
 '''
 for x in range(1,4):
 	if os.system("sudo apt-get install -y libhdf5-dev") == 0:   ####
