a2f341ebf71f723f3ff0c0a428937a65ade30389
brown <yellow@browns-MacBook-Pro.local>
2020-01-25 21:06:40 -0500

    formating

--- Adeept_RaspTank/Controller/controller_setup.py
+++ Adeept_RaspTank/Controller/controller_setup.py
@@ -19,22 +19,22 @@ def replace_num(file,initial,new_num):
             newline += line
     with open(file,"w") as f:
         f.writelines(newline)
-'''
-for x in range(1,4):
-	if os.system(" apt-get update") == 0:
-		break
+# '''
+# for x in range(1,4):
+# 	if os.system(" apt-get update") == 0:
+# 		break
 
-os.system(" apt-get purge -y wolfram-engine")
-os.system(" apt-get purge -y libreoffice*")
-os.system(" apt-get -y clean")
-os.system(" apt-get -y autoremove")
+# os.system(" apt-get purge -y wolfram-engine")
+# os.system(" apt-get purge -y libreoffice*")
+# os.system(" apt-get -y clean")
+# os.system(" apt-get -y autoremove")
 
+# for x in range(1,4):
+# 	if os.system(" apt-get -y upgrade") == 0:
+# 		break
+# '''
 for x in range(1,4):
-	if os.system(" apt-get -y upgrade") == 0:
-		break
-'''
-for x in range(1,4):
-	if os.system(" apt-get install -y i2c-tools") == 0:
+	if os.system(" sudo apt-get install -y i2c-tools") == 0:
 		break
 
 for x in range(1,4):
@@ -61,35 +61,35 @@ for x in range(1,4):
 # for x in range(1,4):
 # 	if os.system(" apt-get install -y libopencv-dev python3-opencv") == 0:
 # 		break
-'''
-for x in range(1,4):
-	if os.system(" apt-get install -y libhdf5-dev") == 0:   ####
-		break
+# '''
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libhdf5-dev") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y libhdf5-serial-dev") == 0:   ####
-		break
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libhdf5-serial-dev") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y build-essential pkg-config") == 0:   ####
-		break
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y build-essential pkg-config") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev") == 0:   ####
-		break
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev") == 0:   ####
-		break
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y libgtk2.0-dev libatlas-base-dev gfortran") == 0:   ####
-		break
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libgtk2.0-dev libatlas-base-dev gfortran") == 0:   ####
+# 		break
 
-for x in range(1,4):
-	if os.system(" apt-get install -y libqtgui4 python3-pyqt5 libqt4-test") == 0:
-		break
-'''
+# for x in range(1,4):
+# 	if os.system(" apt-get install -y libqtgui4 python3-pyqt5 libqt4-test") == 0:
+# 		break
+# '''
 for x in range(1,4):
 	if os.system(" pip3 install imutils pyzmq pybase64 psutil") == 0:   ####
 		break
@@ -109,17 +109,17 @@ except:
 	pass
 
 for x in range(1,4):
-	if os.system(" apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
+	if os.system(" sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
 		break
-'''
-try:
-	os.system(' mkdir //home/pi/.config/autostart')
-	os.system(' touch //home/pi/.config/autostart/car.desktop')
-	with open("//home/pi/.config/autostart/car.desktop",'w') as file_to_write:
-		file_to_write.write("[Desktop Entry]\n   Name=Car\n   Comment=Car\n   Exec=sudo python3 //home/pi/Documents/Rover/Adeept_Rasptank/server/server.py\n   Icon=false\n   Terminal=false\n   MutipleArgs=false\n   Type=Application\n   Catagories=Application;Development;\n   StartupNotify=true")
-except:
-	pass
-'''
+# '''
+# try:
+# 	os.system(' mkdir //home/pi/.config/autostart')
+# 	os.system(' touch //home/pi/.config/autostart/car.desktop')
+# 	with open("//home/pi/.config/autostart/car.desktop",'w') as file_to_write:
+# 		file_to_write.write("[Desktop Entry]\n   Name=Car\n   Comment=Car\n   Exec=sudo python3 //home/pi/Documents/Rover/Adeept_Rasptank/server/server.py\n   Icon=false\n   Terminal=false\n   MutipleArgs=false\n   Type=Application\n   Catagories=Application;Development;\n   StartupNotify=true")
+# except:
+# 	pass
+# '''
 # try:
 # 	os.system('sudo touch //home/pi/startup.sh')
 # 	with open("//home/pi/startup.sh",'w') as file_to_write:
