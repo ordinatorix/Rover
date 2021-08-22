27fa01f4180b0a83b0e14dd7269a7d28a1e8436d
yellow <8978497+seb3n@users.noreply.github.com>
2021-08-22 17:39:57 -0400

    updated path

--- server/servo.py
+++ server/servo.py
@@ -18,7 +18,7 @@ pwm.set_pwm_freq(50)
 
 def num_import_int(initial):		#Call this function to import data from '.txt' file
 	global r
-	with open("$HOME/config.txt") as f:
+	with open("//home/pi/config.txt") as f:
 		for line in f.readlines():
 			if(line.find(initial) == 0):
 				r=line
